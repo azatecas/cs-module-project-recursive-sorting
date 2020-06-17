7b0fafa4d3ad7b03ed63d9bf56cd08105e9cf314
Luis PM <luisp237@gmail.com>
2020-06-17 16:29:44 -0400

    pushing again, sorting solutions not pushed on first commit

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,14 +4,36 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
-
+    for i in range(len(merged_arr)):
+        if len(arrA) > 0 and len(arrB) > 0:
+            if arrA[0] > arrB[0]:
+                merged_arr[i] = arrB.pop(0)
+            else:
+                merged_arr[i] = arrA.pop(0)
+        else:
+            if len(arrA) == 0:
+                merged_arr[i] = arrB.pop(0)
+            else:
+                merged_arr[i] = arrA.pop(0)
 
     return merged_arr
 
-# TO-DO: implement the Merge Sort function below recursively
+# TO-DO: implement the Merge Sort function below recursively 
 def merge_sort(arr):
     # Your code here
+    if len(arr) == 0:
+        return arr
+    if len(arr) == 1:
+        return arr
+    else:
+        mid = len(arr) // 2
 
+        arrA = merge_sort(arr[mid:])
+        arrB = merge_sort(arr[:mid])
+
+        sorted_arr = merge(arrA, arrB)
+
+        return sorted_arr
 
     return arr
 
@@ -19,10 +41,13 @@ def merge_sort(arr):
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
+# def merge_in_place(arr, start, mid, end):
     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
+# def merge_sort_in_place(arr, l, r):
     # Your code here
 
+
+
+#text for commit
